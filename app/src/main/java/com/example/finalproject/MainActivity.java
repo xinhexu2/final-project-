4769dfb2191daefa6ef19a9f4288a78ae04c1dcb
mac <xinhexu2@illinois.edu>
2019-04-20 17:05:05 -0500

    fianl 4/20/17:05

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
