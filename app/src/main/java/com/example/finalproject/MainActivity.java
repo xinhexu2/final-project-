406a702262d1ac7346ea5a75f84ccb26c0f0637d
mac <xinhexu2@illinois.edu>
2019-04-20 22:23:04 -0500

    fianl 4/20/22:22

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -7,6 +7,15 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 import unirest.HttpResponse;
 import unirest.JsonNode;
 import unirest.Unirest;
@@ -18,13 +27,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        HttpResponse<JsonNode> response = Unirest.get("http://data.fixer.io/api/latest")
-                .queryString("access_key", "0c303d204d16a5cb605926b280feda14").asJson();
-        Log.v("Debug", response.toString());
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://data.fixer.io/api/latest?access_key=0c303d204d16a5cb605926b280feda14";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d("Debug", response);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Debug", error.toString());
+
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
 
 
-        EditText dollar = (EditText) findViewById(R.id.input);
-        Double inputAmount = Double.valueOf(dollar.getText().toString());
+//        EditText dollar = (EditText) findViewById(R.id.input);
+//        Double inputAmount = Double.valueOf(dollar.getText().toString());
 //        Double JPYoutput = inputAmount * response;
 //        Toast.makeText(getApplicationContext(), JPYoutput.toString(), Toast.LENGTH_LONG).show();
 
